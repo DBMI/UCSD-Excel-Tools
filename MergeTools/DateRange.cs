419c94a89fe33e7181a595920a6ea61018bc2762
kevin-dbmi <kjdelaney@ucsd.edu>
2026-04-14 12:02:04 -0700

    Add Merge app

---
+++ MergeTools/DateRange.cs
@@ -0,0 +1,224 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace MergeTools
+{
+    internal class DateRange
+    {
+        private DateTime _start;
+        private DateTime _end;
+        private const int oneDay = 1;
+
+        internal DateRange(DateTime start, DateTime end)
+        {
+            // Don't allow reversed dates (start > end).
+            if (start <= end)
+            {
+                _start = start;
+                _end = end;
+            }
+            else
+            {
+                // Automatically swap if they're reversed.
+                _start = end;
+                _end = start;
+            }
+        }
+
+        /// <summary>
+        /// Creates a DateRange object from string like '01/05-01/11'.
+        /// </summary>
+        /// <param name="dateContent">string</param>
+        internal DateRange(string dateContent, int assumedYear)
+        {
+            Regex regex = new Regex(@"(?<month>\d{1,2})\/(?<day>\d{1,2})");
+            string[] dateParts = dateContent.Split('-');
+
+            if (dateParts.Length == 2) 
+            {
+                Match start_match = regex.Match(dateParts[0]);
+
+                if (start_match.Success)
+                {
+                    if (int.TryParse(start_match.Groups["day"].Value, out int day) &&
+                        int.TryParse(start_match.Groups["month"].Value, out int month))
+                    {
+                        _start = new DateTime(assumedYear, month, day);
+                    }                    
+                }
+
+                Match end_match = regex.Match(dateParts[1]);
+
+                if (end_match.Success)
+                {
+                    if (int.TryParse(end_match.Groups["day"].Value, out int day) &&
+                        int.TryParse(end_match.Groups["month"].Value, out int month))
+                    {
+                        _end = new DateTime(assumedYear, month, day);
+                    }
+                }
+
+                // Special handling for end of the year like: "12/29-01/04"
+                if (_start > _end)
+                {
+                    _start = _start.AddYears(-1);   // Move it to previous year.
+                }
+            }
+        }
+
+        /// <summary>
+        /// Bump the year by one 
+        /// (for when we discover after the fact that we used the wrong value for assumedYear.)
+        /// </summary>
+
+        internal void AddYear()
+        {
+            _start = _start.AddYears(1);
+            _end = _end.AddYears(1);
+        }
+
+        /// <summary>
+        /// Is THIS object entirely AFTER this other DateRange object?
+        /// We'll tolerate differences of one day.
+        /// So we'll consider that a DateRange that starts on 01 June
+        /// is NOT "after" a DateRange that ends on 31 May.
+        /// </summary>
+        /// <param name="newDateRange">Another DateRange object.</param>
+
+        private bool After(DateRange newDateRange)
+        {
+            if (newDateRange == null)
+            {
+                return false;
+            }
+
+            return (this.Start() - newDateRange.End()).Days > oneDay;
+        }
+
+        /// <summary>
+        /// Is THIS object entirely BEFORE this other DateRange object?
+        /// We'll tolerate differences of one day.
+        /// So we'll consider that a DateRange that ends on 31 May
+        /// is NOT "before" a DateRange that starts on 01 June.
+        /// </summary>
+        /// <param name="newDateRange">Another DateRange object.</param>
+
+        private bool Before(DateRange newDateRange)
+        {
+            if (newDateRange == null)
+            {
+                return false;
+            }
+
+            return (newDateRange.Start() - this.End()).Days > oneDay;
+        }
+
+        /// <summary>
+        /// Can this DateRange be combined with another DateRange object?
+        /// </summary>
+        /// <param name="newDateRange">Another DateRange object.</param>
+        /// <param name="spanGaps">If there's a gap in date coverage, do we merge the objects? (default: false)</param>
+        
+        internal bool Contiguous(DateRange newDateRange, bool spanGaps = false)
+        {
+            if (spanGaps)
+            {
+                return true;
+            }
+
+            // Test for disjoint date ranges,
+            // in which this object is entirely BEFORE or AFTER newDateRange object.
+            return !(this.After(newDateRange)) && !(this.Before(newDateRange));
+        }
+
+        internal DateTime End()
+        {
+            return _end;
+        }
+
+        internal bool Valid()
+        {
+            return _start != null && _end != null;
+        }
+
+        /// <summary>
+        /// Combine with another DateRange object.
+        /// </summary>
+        /// <param name="newDateRange">Another DateRange object.</param>
+        
+        internal DateRange Merge(DateRange newDateRange)
+        {
+
+            DateTime earlierStart;
+            DateTime laterEnd;
+
+            if (_start <= newDateRange.Start())
+            {
+                earlierStart = _start;
+            }
+            else
+            {
+                earlierStart = newDateRange.Start();
+            }
+
+            if (_end >= newDateRange.End())
+            {
+                laterEnd = _end;
+            }
+            else
+            {
+                laterEnd = newDateRange.End();
+            }
+
+            return new DateRange(earlierStart, laterEnd);
+        }
+
+        public static bool operator >(DateRange r1, DateRange r2)
+        {
+            if ((object)r1 == null)
+            {
+                return false;
+            }
+
+            if ((object)r2 == null)
+            {
+                return true;
+            }
+
+            return r1.Start() > r2.End();
+        }
+
+        public static bool operator <(DateRange r1, DateRange r2)
+        {
+            if ((object)r1 == null)
+            {
+                if ((object)r2 == null)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            if ((object)r2 == null)
+            {
+                return false;
+            }
+
+            return r1.End() < r2.Start();
+        }
+
+        internal DateTime Start()
+        {
+            return _start;
+        }
+    }
+}
---
+++ MergeTools/FileMerger.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Application = Microsoft.Office.Interop.Excel.Application;
+
+namespace MergeTools
+{
+    internal class FileMerger
+    {
+        private Application application;
+        private Dictionary<string, int> columnIndices;
+        private bool firstFile = true;
+        private string folder = String.Empty;
+        private Range target;
+        private const int widthDateTimeColumn = 20;
+        private const int widthIdColumn = 10;
+        private const int widthMetricNameColumn = 40;
+        private const int widthMetricDescripColumn = 25;
+        private const int widthEncounterColumn = 20;
+
+        internal FileMerger()
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                // Because we don't specify an opening directory,
+                // the dialog will open in the last directory used.
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Get the path of specified folder.
+                    folder = folderDialog.SelectedPath;
+                }
+            }
+
+            application = Globals.ThisAddIn.Application;
+            columnIndices = new Dictionary<string, int>();
+        }
+
+        private void DiscoverColumnStartIndices(string line)
+        {
+            columnIndices["NOTE_ID"] = line.IndexOf("NOTE_ID");
+            columnIndices["EFF_LOCAL_DTTM"] = line.IndexOf("EFF_LOCAL_DTTM");
+            columnIndices["METRIC_NAME"] = line.IndexOf("METRIC_NAME");
+            columnIndices["METRIC_DESC"] = line.IndexOf("METRIC_DESC");
+            columnIndices["PAT_ENC_CSN_ID"] = line.IndexOf("PAT_ENC_CSN_ID");
+        }
+
+        private void LabelFile()
+        {
+            target.Offset[0, 0].Value = "NOTE_ID";
+            target.Offset[0, 1].Value = "EFF_LOCAL_DTTM";
+            target.Offset[0, 2].Value = "METRIC_NAME";
+            target.Offset[0, 3].Value = "METRIC_DESC";
+            target.Offset[0, 4].Value = "PAT_ENC_CSN_ID";
+            target = target.Offset[1, 0];
+        }
+
+        internal void Merge(Microsoft.Office.Interop.Excel.Worksheet worksheet)
+        {
+            if (!string.IsNullOrEmpty(folder))
+            {
+                target = (Range)worksheet.Cells[1, 1];
+
+                // Find all the .csv files in the folder.
+                List<string> csvFiles = Directory.GetFiles(folder, "*.csv").ToList<string>();
+
+                foreach (string csvFile in csvFiles) 
+                {
+                    ReadFile(csvFile);
+                }
+
+                application.StatusBar = "Complete.";
+            }
+        }
+
+        internal void ReadFile(string path)
+        {
+            application.StatusBar = "Reading " + path + ".";
+            // Read lines lazily.
+            IEnumerable<string> lines = File.ReadLines(path);
+            bool foundPayload = false;
+
+            // Process each line in a loop.
+            foreach (string line in lines)
+            {
+                if (!foundPayload) 
+                {
+                    if (line.StartsWith("NOTE_ID"))
+                    {
+                        foundPayload = true;
+
+                        if (firstFile)
+                        {
+                            DiscoverColumnStartIndices(line);
+                            LabelFile();
+                            firstFile = false;
+                        }
+                    }
+
+                    continue;
+                }
+
+                if (line.StartsWith("-----"))
+                {
+                    continue;
+                }
+
+                try 
+                {
+                    target.Offset[0, 0].Value2 = line.Substring(columnIndices["NOTE_ID"], widthIdColumn);
+                    target.Offset[0, 1].Value2 = line.Substring(columnIndices["EFF_LOCAL_DTTM"], widthDateTimeColumn);
+                    target.Offset[0, 2].Value2 = line.Substring(columnIndices["METRIC_NAME"], widthMetricNameColumn);
+                    target.Offset[0, 3].Value2 = line.Substring(columnIndices["METRIC_DESC"], widthMetricDescripColumn);
+                    target.Offset[0, 4].Value2 = line.Substring(columnIndices["PAT_ENC_CSN_ID"], widthEncounterColumn);
+                    target = target.Offset[1, 0];
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    // Ran out of data.
+                    return;
+                }                
+            }
+        }
+    }
+}
